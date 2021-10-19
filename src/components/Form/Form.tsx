5ce075973772702f297cbc4f6dac29c19d65a3f7
dansilvaUT <dan13silv@gmail.com>
2021-10-18 21:29:22 -0600

    added error and delete function.

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,19 +1,39 @@
-import { useContext, useState } from "react";
+import { useContext, useState, useEffect } from "react";
 import { AppContext } from "../../context/AppProvider";
 
 const Form = () => {
   const { addItem } = useContext(AppContext);
 
   const [item, setItem] = useState<string>("");
+  const [error, setError] = useState<string>("");
+
+  useEffect(() => {
+    if (error) {
+      setTimeout(() => {
+        setError("");
+      }, 1000);
+    }
+  }, [error]);
+
+  const addListItem = (todo: string) => {
+    if (!todo) {
+      setError("You can't submit an empty todo!");
+    } else {
+      addItem!(todo);
+      setItem("");
+    }
+  };
+
   return (
     <>
+      {error && <span>{error}</span>}
       <input
         type="text"
         placeholder="What's your todo?"
         onChange={(e) => setItem(e.target.value)}
         value={item}
       />
-      <button type="submit" onClick={() => addItem!(item)}>
+      <button type="submit" onClick={() => addListItem(item)}>
         Add Item
       </button>
     </>
